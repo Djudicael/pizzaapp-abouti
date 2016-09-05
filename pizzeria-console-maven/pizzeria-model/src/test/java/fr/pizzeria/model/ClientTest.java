264a98c6a30e10e84f2bc710a81c5f3c2acdb28d
Dubray Judicael <djmx972@gmail.com>
2016-09-05 14:34:56 +0200

    ajout des test unitaire pour les debit et les credit client

---
+++ pizzeria-console-maven/pizzeria-model/src/test/java/fr/pizzeria/model/ClientTest.java
@@ -0,0 +1,48 @@
+package fr.pizzeria.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientTest {
+	private Client client;
+
+	@Before
+	public void setUp() {
+		client = new Client(2, "janel", "panel", 2000);
+
+	}
+
+	@Test(expected = CreditException.class)
+	public void tesCrediterCompte() throws CreditException {
+		client.crediterCompte(5000);
+
+		Assert.assertTrue(client.getSolde().equals(7000.0));
+
+	}
+
+	@Test()
+	public void tesCrediterCompte1() throws CreditException {
+		client.crediterCompte(300);
+
+		Assert.assertTrue(client.getSolde().equals(2300.0));
+
+	}
+
+	@Test(expected = DebitException.class)
+	public void debiterCompte1() throws DebitException {
+		client.debiterCompte(5000);
+
+		Assert.assertTrue(client.getSolde().equals(-3000.0));
+
+	}
+
+	@Test()
+	public void debiterCompte() throws DebitException {
+		client.debiterCompte(300);
+
+		Assert.assertTrue(client.getSolde().equals(1700.0));
+
+	}
+
+}
